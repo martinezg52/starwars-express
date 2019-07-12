b6b0d4655bca2412bc684c1b33bd40d806987f6d
geo_ep <geoffrey.xwin@gmail.com>
2019-07-11 20:58:34 -0700

    extended ver

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -50,6 +50,10 @@ app.get("/add", function(req, res) {
   res.sendFile(path.join(__dirname, "add.html"));
 });
 
+app.get("/all", function(req, res) {
+  res.sendFile(path.join(__dirname, "all.html"));
+});
+
 // Displays all characters
 app.get("/api/characters", function(req, res) {
   return res.json(characters);
@@ -74,17 +78,17 @@ app.get("/api/characters/:character", function(req, res) {
 app.post("/api/characters", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newCharacter = req.body;
+  var newcharacter = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newCharacter);
+  console.log(newcharacter);
 
-  characters.push(newCharacter);
+  characters.push(newcharacter);
 
-  res.json(newCharacter);
+  res.json(newcharacter);
 });
 
 // Starts the server to begin listening
